b2068bdbd565185cea7585a29b7a879cb19cebf2
sanskar sonwal <sanskar006@gmail.com>
2025-12-12 01:09:48 +0530

    day2

---
+++ day2/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    println!("{}", "this is another line in rust");
+    println!("this is the {}rd line",3);
+
+    println!("hello {0}, this is {1}", "skarekroe", "sanjana speaking");
+
+    println!("base 2(binary): {:b}", 3237);
+
+    println!("{number:>5}", number=1);
+    println!("{number:0>5}", number=1);
+    println!("{number:0<5}", number=1);
+}
