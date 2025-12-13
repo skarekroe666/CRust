3e0e20c0231999ba8df23ff4c7d6f17a2947f2c5
sanskar sonwal <sanskar006@gmail.com>
2025-12-13 23:40:59 +0530

    game

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,33 @@
+use rand::{Rng, rng};
+use std::cmp::Ordering;
 use std::io::stdin;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess:");
 
-    let mut guess = String::new();
+    let secret_num = rng().random_range(1..=10);
 
-    stdin().read_line(&mut  guess).expect("Failed to read line");
+    loop {
+        println!("Please input your guess:");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)=> continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
