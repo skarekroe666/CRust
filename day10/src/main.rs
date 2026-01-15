c01c07a638a39f2d4ffdf315763592c57c0d2380
sanskar sonwal <sanskar006@gmail.com>
2026-01-15 13:14:53 +0000

    reference permission

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -18,8 +18,32 @@ fn main() {
     let v = vec![0, 1, 2];
     let n = get_first(&v);
     println!("{} {}", n, v[1]);
+    println!("--------------------------------------------------------");
+
+    borrow_checker();
+    println!("--------------------------------------------------------");
+
+    let mut count = 1;
+    let count_ref = &count;
+    log_counter(count_ref);
+
+    count += 1;
+    println!("Updated counte: {count}");
 }
 
 fn get_first(vr: &Vec<i32>) -> i32 {
     vr[0]
 }
+
+fn borrow_checker() {
+    let mut v = vec![1,2,3];
+    let num = &v[2];
+    println!("Third element is {}", *num);
+    
+    v.push(4);
+}
+
+fn log_counter(num: &i32) {
+    println!("Current count value: {num}");
+}
+
