d30c9a798167b43bd21d3615b7f959ed07f7ff50
sanskar sonwal <sanskar006@gmail.com>
2026-01-22 13:08:11 +0000

    day14

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 10,
+        height: 5,
+    };
+    println!("The area of the rectangle is {}", area(&rect1));
+    println!("{rect1:?}");
+    println!("{rect1:#?}");
+
+    dbg!(&rect1);
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.height * rect.width
+}
+
