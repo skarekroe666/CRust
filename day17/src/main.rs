822408d3e37adaab9e8f6c241bf5fe34d2f7814b
sanskar sonwal <sanskar006@gmail.com>
2026-01-27 09:29:13 +0000

    day18

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -28,13 +28,20 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny(state) => {
             println!("This penny is from {:?}", state);
             1
-        },
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter(rarity) => {
             println!("You got a {:?} quarter!", rarity);
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
@@ -50,16 +57,27 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     println!("{:?}", six);
-    
+
     let no_value: Option<i32> = None;
     let result = plus_one(no_value);
     println!("{:?}", result);
 
+    println!("--------------------------------------------------------------------");
+
+    let dice_roll = 3;
+    match dice_roll {
+        3 => add_hp(),
+        6 => remove_hp(),
+        _ => (),
+    }
+
+    // println!("You got: {}", dice_roll);
 }
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
+fn add_hp() {
+    println!("you got extra hp");
+}
+
+fn remove_hp() {
+    println!("you lost some hp");
 }
