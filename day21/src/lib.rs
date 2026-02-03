3be99548f85ad26a23dff012eb5505e6a77f680c
sanskar sonwal <sanskar006@gmail.com>
2026-02-03 07:55:31 +0000

    day21

---
+++ day21/src/lib.rs
@@ -0,0 +1,59 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {
+        println!("Fixed incorrect order, Here's your meal");
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like a {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
--- day21/target/debug/deps/day21-e770c66f16bec896.d
+++
@@ -1,5 +0,0 @@
-/home/skarekroe/code/rust/learning/day21/target/debug/deps/day21-e770c66f16bec896.d: src/main.rs
-
-/home/skarekroe/code/rust/learning/day21/target/debug/deps/libday21-e770c66f16bec896.rmeta: src/main.rs
-
-src/main.rs:
