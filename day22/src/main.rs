a22eba24104c1821f0df1b6fbddc2bb92de719b8
sanskar sonwal <sanskar006@gmail.com>
2026-02-04 13:16:07 +0000

    collections

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, skarekroe!");
+
+    let mut map = HashMap::new();
+    map.insert("key", "value");
+    println!("{:?}", map);
 }
