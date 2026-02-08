e2a80429ca23eaba5e20dd6854fb76722d6401f0
sanskar sonwal <sanskar006@gmail.com>
2026-02-08 18:50:35 +0530

    day23

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -5,5 +5,35 @@ fn main() {
 
     let mut map = HashMap::new();
     map.insert("key", "value");
-    println!("{:?}", map);
+    println!("{:?}\n", map);
+
+    // let mut count = 11;
+
+    // while count > 1 {
+    //     count -= 1;
+
+    //     if count % 2 == 0 {
+    //         continue;
+    //     }
+
+    //     println!("{count}");
+    // }
+
+    let nums: [i32; 5] = [1, 2, 3, 4, 5];
+
+    for n in nums {
+        let squared = n.pow(2);
+        println!("{n}: {squared}");
+    }
+
+    let names = ["skarekroe", "sanjana", "anisha"];
+
+    // while index < names.len() {
+    //     println!("{}", names[index]);
+    //     index += 1;
+    // }
+
+    for name in names {
+        println!("{name}");
+    }
 }
